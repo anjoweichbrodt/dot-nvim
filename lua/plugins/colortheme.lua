93649df949dfe179a21ed190cd2e7b2fcd05c035
anjoweichbrodt <anjo.weichbrodt@protonmail.com>
2024-03-03 08:27:29 +0100

    Set initial color theme

---
+++ lua/plugins/colortheme.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = true,
+  opts = {
+    style = "night",
+    styles = {
+      functions = {},
+    },
+    on_colors = function(colors)
+      -- colors.bg = "#24283b"
+    end,
+  },
+}
